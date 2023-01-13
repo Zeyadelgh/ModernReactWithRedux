9ea850afda0b65dc1c5ac474155fd291b1080569
Zeyadelgh <49723078+Zeyadelgh@users.noreply.github.com>
2023-01-12 23:43:01 -0500

    added AnimalShow component

--- animals/src/AnimalShow.tsx
+++ animals/src/AnimalShow.tsx
@@ -1,3 +1,3 @@
-export default function AnimalShow() {
-  return <div>Cow!</div>;
+export default function AnimalShow({ type }: { type: string }) {
+  return <div>{type}</div>;
 }
--- animals/src/App.tsx
+++ animals/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 export default function App() {
+  const [animals, setAnimals] = useState<string[]>([]);
   const handleClick = () => {
-    console.log('Button was clicked!');
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
