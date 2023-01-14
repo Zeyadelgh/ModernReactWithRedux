de804100b821dcf9ccf208c4c47a82b7463984b8
Zeyadelgh <49723078+Zeyadelgh@users.noreply.github.com>
2023-01-13 19:35:15 -0500

    added handleClick in Animalshow

--- animals/src/AnimalShow.tsx
+++ animals/src/AnimalShow.tsx
@@ -1,3 +1,5 @@
+import { click } from '@testing-library/user-event/dist/click';
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
@@ -15,11 +17,16 @@ const svgMap: { [index: string]: string } = {
   heart,
   horse,
 };
-
 export default function AnimalShow({ type }: { type: string }) {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img alt='animal' src={svgMap[type]} />
+      <img alt='heart' src={heart} style={{ width: 10 + 10 * clicks + 'px' }} />
     </div>
   );
 }
