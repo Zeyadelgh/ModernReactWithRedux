61ccd25d94570877dbda2af9082f291dc55cbf62
Zeyadelgh <49723078+Zeyadelgh@users.noreply.github.com>
2023-01-18 23:13:42 -0500

    done for the day

--- books/src/components/BookCreate.tsx
+++ books/src/components/BookCreate.tsx
@@ -15,10 +15,13 @@ export default function BookCreate({ onCreate }: AppProps) {
     setTitle('');
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Title</label>
-      <input value={title} onChange={handleChange} />
-      <button>Create!</button>
-    </form>
+    <div className='book-create'>
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className='input' value={title} onChange={handleChange} />
+        <button className='button'>Create!</button>
+      </form>
+    </div>
   );
 }
