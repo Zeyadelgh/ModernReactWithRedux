c64488d84a1a7ee9b47cf236f6e7a2c111164905
Zeyadelgh <49723078+Zeyadelgh@users.noreply.github.com>
2023-01-22 14:16:45 -0500

    continue deleting book by id

--- books/src/App.tsx
+++ books/src/App.tsx
@@ -9,6 +9,12 @@ interface IBooks {
 export default function App() {
   const [books, setBooks] = useState<IBooks[]>([]);
 
+  const deleteBookById = (id: Number) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
   const createBook = (title: string) => {
     const updatedBooks = [
       ...books,
